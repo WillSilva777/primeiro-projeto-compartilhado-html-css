50227e43b307845bf5fe9962cd331b6b13011308
Vinicius Imhof Waldrigues <vinicius.waldrigues@unifebe.edu.br>
2025-11-27 14:25:04 -0300

    Finalizada tela de login e iniciadas telas de favoritos e carrinho

---
+++ assets/javascript/login.js
@@ -0,0 +1,16 @@
+
+
+function logar (){
+
+    var login = document.getElementById("user").value;
+    var senha = document.getElementById("senha").value;
+
+
+    if (login=="admin"&&senha=="admin") {
+        alert("Login concluído com sucesso");
+        location.href = "../index.html";
+    } else {
+        alert("Usuário ou senha incorretos");
+    }
+
+}
